26380ded731be810ac83efbfe32a10a62864ee5e
Roman <ram219219@gmail.com>
2022-10-30 13:45:52 +0300

    Added button

--- app/src/main/java/com/rg/nomadvpn/MainActivity.java
+++ app/src/main/java/com/rg/nomadvpn/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+        /*
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
