92505c9c6f5b250da5fe88fc8428630630112688
Roman <ram219219@gmail.com>
2022-12-10 13:41:51 +0300

    Added

--- app/src/main/java/com/rg/nomadvpn/utils/MyApplication.java
+++ app/src/main/java/com/rg/nomadvpn/utils/MyApplication.java
@@ -1,12 +1,28 @@
 package com.rg.nomadvpn.utils;
 
 import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.rg.nomadvpn.MainActivity;
 
 public class MyApplication extends Application {
+    private AppOpenManager appOpenManager;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         MyApplicationContext myApplicationContext = new MyApplicationContext(this);
+
+        MobileAds.initialize(MyApplicationContext.getAppContext(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+                appOpenManager = new AppOpenManager(MyApplication.this);
+            }
+        });
     }
 }
