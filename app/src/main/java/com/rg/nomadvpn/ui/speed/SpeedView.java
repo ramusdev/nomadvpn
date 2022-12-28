dfe989a4ffa474b85c5d4f554c5e83d469bae1af
Roman <ram219219@gmail.com>
2022-12-28 20:19:29 +0300

    Added

--- app/src/main/java/com/rg/nomadvpn/ui/speed/SpeedView.java
+++ app/src/main/java/com/rg/nomadvpn/ui/speed/SpeedView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -23,13 +24,11 @@ import com.rg.nomadvpn.R;
 import com.rg.nomadvpn.utils.MyApplicationContext;
 
 public class SpeedView extends View {
-
-    int x = 0;
-    float radius = 0;
     float cx = 0;
     float cy = 0;
-    int value = 0;
-    int valueUpload = 0;
+    int valueDownload = 20;
+    int valueUpload = 20;
+    float radius = 350f;
     private Canvas canvas;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private int colorUpload;
@@ -59,90 +58,71 @@ public class SpeedView extends View {
         super.onDraw(canvas);
         canvas.save();
         this.canvas = canvas;
+        radius = 350f;
         // canvas.scale(getWidth(), getHeight());
-        canvas.translate(getPivotX(), getPivotY() / 2);
-
-        // paint.setColor(0x40000000);
-        // paint.setStyle(Paint.Style.FILL);
-        // canvas.drawCircle(0, 0, 1, paint);
-
-        float width = getWidth();
-        float height = getHeight();
-        cx = 0;
-        cy = 0;
-
-        if (width > height) {
-            radius = height / 4;
-        } else {
-            radius = width / 4;
-        }
-
-        /*
-        paint.setColor(Color.parseColor("#352457"));
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(20.0f);
-        canvas.drawCircle(cx, cy, radius, paint);
-        */
+        canvas.translate(getPivotX(), getHeight() / 2);
 
         // Draw first circle
-        paint.setColor(Color.parseColor("#352457"));
+        paint.setColor(Color.parseColor("#364049"));
         paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
-        paint.setStrokeWidth(20.0f);
+        paint.setStrokeWidth(80.0f);
         RectF ovalFirst = new RectF();
         ovalFirst.set(cx - radius , cy - radius, cx + radius, cy + radius);
         canvas.drawArc(ovalFirst, 140, 260, false, paint);
 
         // Draw second circle
-        paint.setColor(Color.parseColor("#221638"));
-        float radiusSecond = radius - 20.0f;
+        paint.setColor(Color.parseColor("#4d364049"));
+        paint.setStrokeWidth(30.0f);
+        float radiusSecond = radius - 55.0f;
         RectF ovalSecond = new RectF();
         ovalSecond.set(cx - radiusSecond , cy - radiusSecond, cx + radiusSecond, cy + radiusSecond);
         canvas.drawArc(ovalSecond, 140, 260, false, paint);
 
         // Draw third
-        paint.setColor(Color.parseColor("#352457"));
-        float radiusThird = radius - 40.0f;
-        RectF ovalThird = new RectF();
-        ovalThird.set(cx - radiusThird , cy - radiusThird, cx + radiusThird, cy + radiusThird);
-        canvas.drawArc(ovalThird, 140, 260, false, paint);
+        // paint.setColor(Color.parseColor("#610c29"));
+        // paint.setStrokeWidth(50.0f);
+        // float radiusThird = radius - 100.0f;
+        // RectF ovalThird = new RectF();
+        // ovalThird.set(cx - radiusThird , cy - radiusThird, cx + radiusThird, cy + radiusThird);
+        // canvas.drawArc(ovalThird, 140, 260, false, paint);
 
-        // paint.setColor(Color.parseColor("#004f53"));
-        // paint.setStyle(Paint.Style.FILL);
-        // canvas.drawCircle(cx - radius, cy, radius - 250.0f, paint);
-
+        // Draw speed download
+        paint.setColor(Color.parseColor("#0fd6b5"));
+        paint.setStrokeWidth(80);
         RectF oval = new RectF();
         oval.set(cx - radius , cy - radius, cx + radius, cy + radius);
-        paint.setColor(Color.parseColor("#004f53"));
-        paint.setStrokeWidth(20);
-        canvas.drawArc(oval, 140, this.value, false, paint);
+        canvas.drawArc(oval, 140, this.valueDownload, false, paint);
 
-        RectF ovalUpload = new RectF();
-        ovalUpload.set(cx - radiusThird , cy - radiusThird, cx + radiusThird, cy + radiusThird);
-        paint.setColor(colorUpload);
-        paint.setStrokeWidth(20);
-        canvas.drawArc(ovalUpload, 140, this.valueUpload, false, paint);
+        // Draw speed upload
+        // paint.setStrokeWidth(50);
+        // paint.setColor(colorUpload);
+        // RectF ovalUpload = new RectF();
+        // ovalUpload.set(cx - radiusThird , cy - radiusThird, cx + radiusThird, cy + radiusThird);
+        // canvas.drawArc(ovalUpload, 140, this.valueUpload, false, paint);
 
         int x = 0;
         int y = 0;
 
-        double radiusText = radius + 45;
-        double angleText = (this.value + 130) % 360;
-        double angleTextDraw = (this.value + 228) % 360;
+        double radiusText = radius - 9;
+        double angleText = (this.valueDownload + 123) % 360;
         double angle = Math.toRadians(angleText);
+        double angleTextDraw = (this.valueDownload + 220) % 360;
         float coordX = (float) (radiusText * Math.cos(angle));
         float coordY = (float) (radiusText * Math.sin(angle));
 
-        paint.setColor(Color.parseColor("#ffffff"));
-        paint.setTextSize(30);
-        paint.setStrokeWidth(1);
+        paint.setColor(Color.parseColor("#0a5145"));
+        paint.setTextSize(34);
+        // paint.setStrokeWidth(20);
+        paint.setStyle(Paint.Style.FILL);
         canvas.rotate((float) angleTextDraw, coordX, coordY);
         canvas.drawText(String.valueOf(angleText), coordX,  coordY, paint);
 
-        Log.d(MainActivity.LOGTAG, "Coord --------------------------------------->");
-        Log.d(MainActivity.LOGTAG,"CoordX: " + coordX);
-        Log.d(MainActivity.LOGTAG,"CoordY: " + coordY);
+
+
+
+
+
 
         canvas.restore();
         // invalidate();
@@ -161,8 +141,8 @@ public class SpeedView extends View {
     }
 
 
-    public void setValue(int value) {
-        this.value = value;
+    public void setValue(int valueDownload) {
+        this.valueDownload = valueDownload;
         invalidate();
     }
 
@@ -172,7 +152,7 @@ public class SpeedView extends View {
     }
 
     public void valueAnimatorStartDownload() {
-        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "value", 0, 260);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "value", 20, 260);
         objectAnimator.setDuration(10000);
         objectAnimator.setInterpolator(new DecelerateInterpolator());
         objectAnimator.start();
@@ -202,7 +182,7 @@ public class SpeedView extends View {
     }
 
     public void valueAnimatorStartUpload() {
-        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "value2", 0, 260);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofInt(this, "value2", 20, 260);
         objectAnimator.setDuration(10000);
         objectAnimator.setInterpolator(new DecelerateInterpolator());
         objectAnimator.start();
